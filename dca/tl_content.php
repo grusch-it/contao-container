68f6394d33463cbddd685d58b7b2b535b54e845a
Michael Gruschwitz <info@grusch-it.de>
2015-04-07 16:27:33 +0200

    Configuration

---
+++ dca/tl_content.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   gruschit_container
+ * @author    Michael Gruschwitz <info@grusch-it.de>
+ * @license   LGPL
+ * @copyright Michael Gruschwitz 2015
+ */
+
+$GLOBALS['TL_DCA']['tl_content']['palettes']['containerStart'] = '{type_legend},type;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['containerStop']  = '{type_legend},type;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests;{invisible_legend:hide},invisible,start,stop';
